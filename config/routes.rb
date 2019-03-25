e984d6672c41b11429ff53af7c03d1d948999610
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-03-25 22:25:54 +0900

    edit-routes
    groupコントローラーに関するルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :messages, only: :index
-  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+  resources :users, only: [:index, :edit, :update]
   root to: 'messages#index'
 end
