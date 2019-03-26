7d755ae969319a0430006b8f034145b6f3757456
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-03-27 00:20:14 +0900

    change-root_path
    ルートパスをmessages#indexから、groups#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  root to: 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  root to: 'messages#index'
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index]
+  end
 end
