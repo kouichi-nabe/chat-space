f6510a5bdbbf4aae3e643eb32d91f698a4dea509
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-04-14 23:30:27 +0900

    Add-incremental_search-ajax
    インクリメンタルサーチのためのjqueryファイルとjbuilderファイルの作成、users#indexの編集

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,63 @@
+$(function(){
+
+  var user_list = $("#user-search-result");
+
+  var group_user_list = $('#chat-group-users')
+
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    user_list.append(html);
+  }
+
+  function appendGroupUser(id, name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    group_user_list.append(html);
+  }
+
+  function appendErrMsgToHTML(msg){
+    var html = `<p class="chat-group-user__name">${ msg }</p>`
+    user_list.append(html);
+  }
+
+  $('#user-search-field').on("keyup", function(){
+    var input = $('#user-search-field').val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: {keyword: input},
+      dataType: 'json'
+    })
+    .done(function(users){
+      $('#user-search-result').empty();
+      if(users.length !== 0 && input.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        })
+      } else {
+        appendErrMsgToHTML("一致するユーザーは見つかりませんでした");
+      }
+    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました");
+    })
+  });
+
+  $(document).on("click", ".user-search-add", function(){
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    $(this).parent().remove();
+    appendGroupUser(id, name);
+  })
+
+  $(document).on("click", ".user-search-remove", function(){
+    $(this).parent().remove();
+  })
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20).select {|user| user[:name] != current_user.name}
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
