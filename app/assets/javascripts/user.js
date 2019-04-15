cd9cca14d2f49de226fe19b881f1f0f26e86bc5d
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-04-15 20:37:36 +0900

    Fix-user.js
    input.length !== 0 の場合には、ajaxを起こさないように修正。また、リンクではなかったため、<a></a>を修正。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,7 +7,7 @@ $(function(){
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
     user_list.append(html);
   }
@@ -16,7 +16,7 @@ $(function(){
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
                   <input name='group[user_ids][]' type='hidden' value='${id}'>
                   <p class='chat-group-user__name'>${name}</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                 </div>`
     group_user_list.append(html);
   }
@@ -29,25 +29,29 @@ $(function(){
   $('#user-search-field').on("keyup", function(){
     var input = $('#user-search-field').val();
 
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: {keyword: input},
-      dataType: 'json'
-    })
-    .done(function(users){
+    if(input.length !== 0){
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: {keyword: input},
+        dataType: 'json'
+      })
+      .done(function(users){
+        $('#user-search-result').empty();
+        if(users.length !== 0 && input.length !== 0){
+          users.forEach(function(user){
+            appendUser(user);
+          })
+        } else {
+          appendErrMsgToHTML("一致するユーザーは見つかりませんでした");
+        }
+      })
+      .fail(function(){
+        alert("ユーザー検索に失敗しました");
+      })
+    } else {
       $('#user-search-result').empty();
-      if(users.length !== 0 && input.length !== 0){
-        users.forEach(function(user){
-          appendUser(user);
-        })
-      } else {
-        appendErrMsgToHTML("一致するユーザーは見つかりませんでした");
-      }
-    })
-    .fail(function(){
-      alert("ユーザー検索に失敗しました");
-    })
+    }
   });
 
   $(document).on("click", ".user-search-add", function(){
