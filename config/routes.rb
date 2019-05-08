f70aa29bd1db6aa0a68879543231c944ec94e386
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-05-08 22:44:50 +0900

    Add-unicorn
    Unicornのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:show, :index, :create]
     namespace :api do
       resources :messages, only: :index, defaults: {format: 'json'}
     end
