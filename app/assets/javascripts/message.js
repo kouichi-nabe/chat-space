19ee5108b3c52423777c504729bdc6513dd20421
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-06-08 19:38:33 +0900

    Fix-message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -26,7 +26,7 @@ $(function(){
     var id = message.id
     var name = message.user_name
     var time = message.created_at;
-    var body = message.body if message.body
+    var body = message.body if message.body != null
     var image = message.image.url ? <img src="${image}" class='message-image'/> : '';
     var html = `<div class="message-list__item" data-id=${id}>
                     <h2 class="message-list__item__user-name">${name}
